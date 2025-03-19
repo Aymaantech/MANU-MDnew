204361a327bf8cf7a2326bc2f797792f718f914a
Aymaantech <zzmix7323@gmail.com>
2025-03-19 08:14:59 +0000

    Update settings.js
    Setting.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === Manu-MD&cbUTGaCT#gz7saSY-L_zQv34scY66qIdPY-bCwTH8Yhi1XKoq8Pw ? '' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
